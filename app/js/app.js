491afc6f0fe4de67c8ec55bd2b97e1a4ca0fd481
Victor Sokoluik <vic.sokoliuk@gmail.com>
2021-06-18 13:50:19 +0300

    the ordering page finished

--- app/js/app.js
+++ app/js/app.js
@@ -79,11 +79,6 @@ if(ascBtn !== null && decsBtn !== null){
     });
 }
 
-
-
-
-
-
 /*** Sort Order END***/
 
 new fullpage('#fullPage', {
@@ -136,6 +131,58 @@ $('.inner-mileage-slider').owlCarousel({
         }
     }
 });
+
+/*** Datepicker **/
+
+$( function() {
+    $("#datepicker").datepicker({
+        dateFormat: "dd.mm.yy"
+    });
+});
+
+/* Russian (UTF-8) initialisation for the jQuery UI date picker plugin. */
+/* Written by Andrew Stromnov (stromnov@gmail.com). */
+( function( factory ) {
+	"use strict";
+
+	if ( typeof define === "function" && define.amd ) {
+
+		// AMD. Register as an anonymous module.
+		define( [ "../widgets/datepicker" ], factory );
+	} else {
+
+		// Browser globals
+		factory( jQuery.datepicker );
+	}
+} )( function( datepicker ) {
+"use strict";
+
+datepicker.regional.ru = {
+	closeText: "Закрыть",
+	prevText: "&#x3C;Пред",
+	nextText: "След&#x3E;",
+	currentText: "Сегодня",
+	monthNames: [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+	"Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ],
+	monthNamesShort: [ "Янв", "Фев", "Мар", "Апр", "Май", "Июн",
+	"Июл", "Авг", "Сен", "Окт", "Ноя", "Дек" ],
+	dayNames: [ "воскресенье", "понедельник", "вторник", "среда", "четверг", "пятница", "суббота" ],
+	dayNamesShort: [ "вск", "пнд", "втр", "срд", "чтв", "птн", "сбт" ],
+	dayNamesMin: [ "Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб" ],
+	weekHeader: "Нед",
+	dateFormat: "dd.mm.yy",
+	firstDay: 1,
+	isRTL: false,
+	showMonthAfterYear: false,
+	yearSuffix: "" };
+datepicker.setDefaults( datepicker.regional.ru );
+
+return datepicker.regional.ru;
+
+} );
+
+/*** Datepicker END ***/
+
 
 function inner_mileage_slider(){
 
